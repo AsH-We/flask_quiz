e0a044e7d6eb5cd56ce78a5132c3ce9427013ece
abilmansur <abilmansur.ashim@gmail.com>
2024-12-22 00:39:49 +0500

    run settings update

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ def next_question():
     return jsonify({'question': questions[question_index]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
